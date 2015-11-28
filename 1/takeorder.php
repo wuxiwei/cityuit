8df385ac586043f1c2c0533859b0b22659744e1c
wxw <wxw_it@163.com>
2015-11-28 15:27:03 +0800

    update

--- 1/takeorder.php
+++ 1/takeorder.php
@@ -17,22 +17,26 @@ try{
     $sql_se = "SELECT * FROM `order_meal` WHERE `ordernum` = '$ordernum' and `orderstate` = 'n'";
     $resultsql = $pdo->query($sql_se);
     if($row = $resultsql->fetch(PDO::FETCH_ASSOC)){     //如果满足条件则抢单成功
-        $time = date('y-m-d h:i:s',time());   //抢单时间
+        $time = date('y-m-d H:i:s',time());   //抢单时间
         $sql_up="UPDATE `order_meal` SET `orderstate` = 's', `ordersuccess` = '$time', `sendmealman` = '$takeorderusername' WHERE `ordernum` = '$ordernum';";  //
         $pdo->exec($sql_up);
         //动作一通知订餐人有人抢单
         $ordermanim=getim($row['ordermealman'],$pdo);    //获取订餐人im帐号
-        $orderInfo = array('object'=>'user','status'=>'taked');     //附加判断条件
-        $takeordermanphone=getphone($takeorderusername,$pdo);    //获取送餐人手机号
-        $takeordermess=json_encode(['username' => $takeorderusername,'phone' => $takeordermanphone], JSON_UNESCAPED_UNICODE);   //字符编码
-        $res = $IM->xx_hxSend([$ordermanim],$takeordermess,$orderInfo,$takeorderusername);
+        if(onOffLine($ordermanim, $IM) == 'online'){    //判断是否在线
+            $orderInfo = array('object'=>'user','status'=>'taked');     //附加判断条件
+            $takeordermanphone=getphone($takeorderusername,$pdo);    //获取送餐人手机号
+            $takeordermess=json_encode(['username' => $takeorderusername,'phone' => $takeordermanphone], JSON_UNESCAPED_UNICODE);   //字符编码
+            $res = $IM->xx_hxSend([$ordermanim],$takeordermess,$orderInfo,$takeorderusername);
+        }
         //print_r($res);
         //动作二通知所有送餐人订单被抢
         $senduser = getAllSendImonline($pdo, $IM);   //获取所有在线送餐人im帐号
-        $orderInfo = array('object'=>'send','status'=>'send');     //附加判断条件
-        $jsordernum = json_encode(['ordernum' => $ordernum], JSON_UNESCAPED_UNICODE);   //字符编码josn格式返回数据订单号
-        $res1 = $IM->xx_hxSend($senduser,$jsordernum,$orderInfo,$takeorderusername);
-        //print_r($res1);
+        if(!empty($senduser)){
+            $orderInfo = array('object'=>'send','status'=>'send');     //附加判断条件
+            $jsordernum = json_encode(['ordernum' => $ordernum], JSON_UNESCAPED_UNICODE);   //字符编码josn格式返回数据订单号
+            $res1 = $IM->xx_hxSend($senduser,$jsordernum,$orderInfo,$takeorderusername);
+            //print_r($res1);
+        }
         //动作三返回告知抢单成功
         $usrInfo = array('status'=>'take success');
         echoinf($usrInfo);
